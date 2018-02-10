918da25ee701b902a4cb967539fd0d5a3b072707
schmidt-aaron <aschmidt808@gmail.com>
2018-02-10 14:02:50 -0800

    25 - added login/auth logic

--- delicious/controllers/authController.js
+++ delicious/controllers/authController.js
@@ -6,3 +6,19 @@ exports.login = passport.authenticate('local', {
   successRedirect: '/',
   successFlash: 'You are now logged in!'
 });
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'You are now logged out!');
+  res.redirect('/');
+}
+
+exports.isLoggedIn = (req, res, next) => {
+  //check if authenicated
+  if(req.isAuthenticated()) {
+    next(); //all is well -- move along
+    return;
+  }
+  req.flash('error', 'Opps you must be logged in');
+  res.redirect('/login');
+}
