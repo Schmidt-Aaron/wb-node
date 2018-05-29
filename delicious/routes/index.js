267b9d0a169d9a8bcca490c054a9379cffa2d69e
schmidt-aaron <aschmidt808@gmail.com>
2018-05-29 14:27:19 -0700

    added reviews model

--- delicious/routes/index.js
+++ delicious/routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
 
 // call the error handler
 const { catchErrors } = require('../handlers/errorHandlers');
@@ -54,7 +55,11 @@ router.post('/account/reset/:token',
 
 router.get('/map', storeController.mapPage);
 router.get('/hearts', authController.isLoggedIn,  catchErrors(storeController.getHearts));
-    
+router.post('/reviews/:id', 
+    authController.isLoggedIn, 
+    catchErrors(reviewController.addReview)
+);
+
 // API routes
 router.get('/api/search', catchErrors(storeController.searchStores));
 router.get('/api/stores/near', catchErrors(storeController.mapStores));
